b85ea7844cc751ce2c7b030d9481dccbec85a81c
parthathalye17 <athalyeparth@gmail.com>
2024-06-21 12:23:38 +0530

    Fraud update

--- backend/parth/models/testing_model.py
+++ backend/parth/models/testing_model.py
@@ -3,12 +3,11 @@ import pandas as pd
 import joblib
 
 new_entry = {
-    "amount": 1500000,
-    "transaction_hour": 1,
+    "amount": 300,
+    "transaction_hour": 20,
     "transaction_day_of_week": 3,
-    "email_domain": 2,
-    "avg_transaction_amount": 1500,
-    "city_consistency": 0.4,
+    "avg_transaction_amount": 521,
+    "city_consistency": 0.9,
     "country_consistency": 1,
     "average_transaction_frequency": 1,
     "transaction_type": 1,
@@ -21,7 +20,7 @@ new_entry_df = pd.DataFrame([new_entry])
 xgb_model = joblib.load('xgb_model.pkl')
 scaler = joblib.load('scaler.pkl')
 
-columns = [ "amount","transaction_hour", "transaction_day_of_week", "email_domain",
+columns = [ "amount","transaction_hour", "transaction_day_of_week",
     "avg_transaction_amount", "city_consistency", "country_consistency", "average_transaction_frequency", "transaction_type",
     "transaction_day", "transaction_month", "transaction_year"
 ]
@@ -29,7 +28,7 @@ new_entry_df = new_entry_df[columns]
 new_entry_scaled = scaler.transform(new_entry_df)
 
 y_pred_prob = xgb_model.predict_proba(new_entry_scaled)[:, 1]
-best_threshold = 0.1
+best_threshold = 0.5
 y_pred = (y_pred_prob > best_threshold).astype(int)
 
 print(f"Predicted probability of fraud: {y_pred_prob[0]}")
